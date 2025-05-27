94f0aa377d281d480598fb2e9c5234dbd1444ba0
Divine Mbamara <divinembamara@gmail.com>
2025-05-27 17:40:41 -0400

    Version 2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-$(document).ready(function() {
-    $("#metadata").load("meta.html");
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-});
+
+function togglePopup1(){
+    document.getElementById("project-popup-1").classList.toggle("active");
+}
+function togglePopup2(){
+    document.getElementById("project-popup-2").classList.toggle("active");
+}
+function togglePopup3(){
+    document.getElementById("project-popup-3").classList.toggle("active");
+}
+function togglePopup4(){
+    document.getElementById("project-popup-4").classList.toggle("active");
+}
+function togglePopup5(){
+    document.getElementById("project-popup-5").classList.toggle("active");
+}
+function togglePopup6(){
+    document.getElementById("project-popup-6").classList.toggle("active");
+}
+function togglePopup7(){
+    document.getElementById("project-popup-7").classList.toggle("active");
+}
+function togglePopup8(){
+    document.getElementById("project-popup-8").classList.toggle("active");
+}
